ca865b6e59c89c379bd0ac3ab482f393f38d8f37
joaquin <jecorrea@protonmail.com>
2021-12-26 19:27:31 -0500

    shorter solution

--- script.js
+++ script.js
@@ -2,14 +2,21 @@
   string is a palindrome string. Palnidrom means the string will read the same from left to right or right to left. 
   Example: level */
 
+// function palindrome(string) {
+//   let ans;
+
+//   for (let index = 0; index < string.length; index++) {
+//     string[index] !== string[string.length - 1 - index]
+//       ? (ans = false)
+//       : (ans = true);
+//   }
+
+//   return ans;
+// }
+
 function palindrome(string) {
-  let ans;
-
-  for (let index = 0; index < string.length; index++) {
-    string[index] !== string[string.length - 1 - index]
-      ? (ans = false)
-      : (ans = true);
+  for (let i = 0; i < string.length; i++) {
+    let ans = string[i] !== string[string.length - 1 - i] ? false : true;
+    return ans;
   }
-
-  return ans;
 }
